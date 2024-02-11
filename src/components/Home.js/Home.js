8efdc2c8faa5df645502d684e5a3bf1f545e345d
saurabh <saurabh2113076@akgec.ac.in>
2024-02-11 22:50:42 +0530

    some changes

--- src/components/Home.js/Home.js
+++ src/components/Home.js/Home.js
@@ -48,7 +48,7 @@ function Home(props) {
     <div className='Home-page' >
     <div className="banner-div">
      <div className="products-Categories" style={{}}>
-     <ul>
+     
         <li><a href="#filter" onClick={()=>setCategory("smartphones")}>Smartphones</a></li>
         <li><a href="#filter" onClick={()=>setCategory("laptops")}>Laptops</a></li>
         <li><a href="#filter" onClick={()=>setCategory("fragrances")}>Fragrances</a></li>
@@ -57,7 +57,7 @@ function Home(props) {
         <li><a href="#filter" onClick={()=>setCategory("groceries")}>Home-decoration</a></li>
         <li><a href="#filter" onClick={()=>setCategory("home-decoration")}>Groceries</a></li>
         <li><a href="#">Contect</a></li>
-      </ul>
+      
 
      </div>
      <div className="banner-container">
